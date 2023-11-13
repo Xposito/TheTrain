d1059614ac599c907ef9ccd7ae9b4939b93e7f3f
kukutras <lucia.tello.miguel@gmail.com>
2023-11-13 13:48:14 +0100

    MENÚS 2
    Se ha programado el menú principal y el de pausa.

---
+++ Assets/Scripts/UI/SCR_ui_pause_menu.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SCR_ui_pause_menu : MonoBehaviour
+{
+    public bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+    public GameObject UI;
+
+
+
+    public void Awake()
+    {
+        GameIsPaused = false;
+        Resume();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        UI.SetActive(true);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        UI.SetActive(false);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("1");
+        SceneManager.LoadScene("1");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting");
+        Application.Quit();
+    }
+}
