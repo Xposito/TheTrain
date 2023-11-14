112497ad524794aed3c00c862dab420bf6f6abd9
Xposito <114917289+Xposito@users.noreply.github.com>
2023-11-14 13:05:42 +0100

    Script_takePhoto

--- Assets/Scripts/Camera/SCR_cam_takePhoto.cs
+++ Assets/Scripts/Camera/SCR_cam_takePhoto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,72 +6,91 @@ using UnityEngine;
 public class SCR_cam_takePhoto : MonoBehaviour
 {
 
-    
+
     //Variables  del Raycast
-    [Header("Variables Raycast")]
-    bool m_Started;
-    public LayerMask m_LayerMask;
-    [SerializeField] float m_scale = 1;
-    [SerializeField] float m_long = 1;
-    [SerializeField] float m_distance = 1;
+    //[Header("Variables Raycast")]
+    //bool m_Started;
+    //public LayerMask m_LayerMask;
+    //[SerializeField] float m_scale = 1;
+    //[SerializeField] float m_long = 1;
+    //[SerializeField] float m_distance = 1;
 
     //Variables para las fotos
-    [Header("Variables Fotos")]
+    //[Header("Variables Fotos")]
+    //[SerializeField] private Renderer[] renderCubo;
+
+    Camera cameraMain;
+    Plane[] cameraFrustum;
+    public GameObject[] gameObjects;
+    public Collider[] collidersProps;
+    Bounds[] collidersBounds;
+    Bounds bound;
+
+
+    public SCR_scr_Player_Options playerOption;
+
     [SerializeField] private Renderer[] renderCubo;
+    private int elementos = 0;
+
+
 
     void Start()
     {
         //Use this to ensure that the Gizmos are being drawn when in Play Mode.
-        m_Started = true;
+        //m_Started = true;
+        cameraMain = GetComponent<Camera>();
+        gameObjects = GameObject.FindGameObjectsWithTag("Prop");
 
-      
-    }
+        collidersProps = new BoxCollider[gameObjects.Length];
+        collidersBounds = new Bounds[gameObjects.Length];
 
-    void FixedUpdate()
-    {
-        MyCollisions();
-    }
-    public void LateUpdate()
-    {
-        if (Input.GetMouseButtonDown(0))
+        for(int i = 0; i < gameObjects.Length; i++)
         {
-            StartCoroutine(RecordFrame());
+            collidersProps[i] = gameObjects[i].GetComponent<Collider>();
+            collidersBounds[i] = collidersProps[i].bounds;
+
         }
+
     }
 
-
-            void MyCollisions()
+    void Update()
     {
-        //Use the OverlapBox to detect if there are any other colliders within this box area.
-        //Use the GameObject's centre, half the size (as a radius) and rotation. This creates an invisible box around your GameObject.
-        Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position + new Vector3(0, 0,0 + m_distance), new Vector3(1,1 ,1 * m_long) * m_scale, Quaternion.identity, m_LayerMask);
-        int i = 0;
-        //Check when there is a new collider coming into contact with the box
-        while (i < hitColliders.Length)
+        
+        if (playerOption.usingCam == true)
         {
-            //Output all of the collider names
-            Debug.Log("Hit : " + hitColliders[i].name + i);
-            //Increase the number of Colliders in the array
-            i++;
-        }
-    }
+            for(int i = 0; i < gameObjects.Length; i++)
+            {
+                bound = collidersProps[i].bounds;
 
-    //Draw the Box Overlap as a gizmo to show where it currently is testing. Click the Gizmos button to see this
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        //Check that it is being run in Play Mode, so it doesn't try to draw this in Editor mode
-        if (m_Started)
-            //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
-            Gizmos.DrawWireCube(gameObject.transform.position + new Vector3(0,0,0 + m_distance), new Vector3(1 ,1,1 * m_long) * m_scale);
+                cameraFrustum = GeometryUtility.CalculateFrustumPlanes(cameraMain);
+                if (GeometryUtility.TestPlanesAABB(cameraFrustum, bound))
+                {
+                    Debug.Log("detecto cubo");
+                    if (Input.GetMouseButtonDown(0))
+                    {
+
+                        StartCoroutine(RecordFrame());
+
+                    }
+                }
+            }
+            
+            
+            
+        }
+        //MyCollisions();
+        
     }
+    
 
     IEnumerator RecordFrame()
     {
         yield return new WaitForEndOfFrame();
         var texture = ScreenCapture.CaptureScreenshotAsTexture();
 
-        renderCubo[1].material.mainTexture = texture;
+        renderCubo[elementos].material.mainTexture = texture;
+        elementos++;
+
         // do something with texture
 
         //// cleanup
