d5105726ffd8f932dd320359c1c94c1095ee54b6
Xposito <114917289+Xposito@users.noreply.github.com>
2024-01-16 14:09:41 +0100

    retoques de scripts

--- Assets/Scripts/Camera/SCR_cam_takePhoto.cs
+++ Assets/Scripts/Camera/SCR_cam_takePhoto.cs
@@ -8,26 +8,8 @@ using static UnityEditor.UIElements.ToolbarMenu;
 
 public class SCR_cam_takePhoto : MonoBehaviour
 {
-
-
-    //Variables  del Raycast
-    //[Header("Variables Raycast")]
-    //bool m_Started;
-    //public LayerMask m_LayerMask;
-    //[SerializeField] float m_scale = 1;
-    //[SerializeField] float m_long = 1;
-    //[SerializeField] float m_distance = 1;
-
-    //Variables para las fotos
-    //[Header("Variables Fotos")]
-    //[SerializeField] private Renderer[] renderCubo;  
-
-    //Camera cameraMain;
-    //Plane[] cameraFrustum;
     [Header("Arrays")]
     public GameObject[] puertas;
-    //Bounds[] collidersBounds;
-    //Bounds bound;
     
     public LayerMask layer;
     public SCR_scr_Player_Options playerOption;
@@ -43,87 +25,15 @@ public class SCR_cam_takePhoto : MonoBehaviour
 
     public Tutorial tutorial;
 
-    void Start()
-    {
-        //Use this to ensure that the Gizmos are being drawn when in Play Mode.
-        //m_Started = true;
-        //cameraMain = GetComponent<Camera>();
-        
-        
-        
-        //collidersBounds = new Bounds[gameObjects.Length];
-
-        //for(int i = 0; i < gameObjects.Length; i++)
-        //{
-        //    collidersProps[i] = gameObjects[i].GetComponent<Collider>();
-            //collidersBounds[i] = collidersProps[i].bounds;
-
-        //}
-
-    }
 
     void Update()
     {
         
         if (playerOption.usingCam == true)
         {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 7, layer))
-            {
-                Debug.Log(hit.transform.name);
-                if (Input.GetMouseButtonDown(0))
-                {
-                    //if (hit.collider.CompareTag("Bells"))
-                    //{
-                    //    Debug.Log("Funciona");
-                    //    animator = puertas[puertasInt].transform.GetComponent<Animator>();
-                        
-                        
-
-                    //    animator.SetBool("AbrirPuerta", true);
-                    //    puertasInt++;
-                    //}
-
-                    Collider collider = hit.transform.GetComponent<Collider>();
-                    collider.enabled = false;
-                    flash.SetActive(false);
-                    tutorial.CambioDeEstado();
-                    StartCoroutine(RecordFrame());
-                }
-            }
-
-            //for(int i = 0; i < gameObjects.Length; i++)
-            //{
-
-                //    bound = collidersProps[i].bounds;
-
-                //    cameraFrustum = GeometryUtility.CalculateFrustumPlanes(cameraMain);
-                //    if (GeometryUtility.TestPlanesAABB(cameraFrustum, bound))
-                //    {
-
-                //        Debug.Log("detecto cubo");
-                //        itsPhoto = true;
-
-                //    }
-                //    else
-                //    {
-                //        itsPhoto = false;
-                //    }
-                //}
-                //if (itsPhoto)
-                //{
-
-                //    if (Input.GetMouseButtonDown(0))
-                //    {
-
-                //        StartCoroutine(RecordFrame());
-
-                //    }
-                //}
-
-
+            UseCamera();
         }
-        //MyCollisions();
+        
         
     }
     
@@ -142,9 +52,22 @@ public class SCR_cam_takePhoto : MonoBehaviour
   
         elementos++;
 
-        // do something with texture
-
-        //// cleanup
-        //Object.Destroy(texture);
+    }
+     
+    void UseCamera()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 7, layer))
+        {
+            Debug.Log(hit.transform.name);
+            if (Input.GetMouseButtonDown(0))
+            {
+                Collider collider = hit.transform.GetComponent<Collider>();
+                collider.enabled = false;
+                flash.SetActive(false);
+                tutorial.CambioDeEstado();
+                StartCoroutine(RecordFrame());
+            }
+        }
     }
 }
--- Assets/Scripts/Puzzles/Puzzle2/SCR_puz_Puzzle2_Base.cs
+++ Assets/Scripts/Puzzles/Puzzle2/SCR_puz_Puzzle2_Base.cs
@@ -7,7 +7,7 @@ public class SCR_puz_Puzzle2_Base : MonoBehaviour
 {
     public SCR_scr_Puzzle_2_Item thisItem;
 
-    // Start is called before the first frame update
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Puzzle2"))
@@ -29,18 +29,5 @@ public class SCR_puz_Puzzle2_Base : MonoBehaviour
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Puzzle2"))
-        {
-            SCR_scr_Puzzle_2_Item objectItem = other.GetComponent<SCR_puz_Puzzle2_Item>().thisItem;
-            if (objectItem)
-            {
-                if (objectItem == thisItem && objectItem.canBeMoved)
-                {
-                    objectItem.correctPlace = false;
-                }
-            }
-        }
-    }
+    
 }
